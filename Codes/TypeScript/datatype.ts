062cb720abb90cdea25704c9080d3727107785c6
Pinnsvin <Pinnsvin@163.com>
2021-03-04 17:09:37 +0800

    add...

---
+++ Codes/TypeScript/datatype.ts
@@ -0,0 +1,7 @@
+enum Color { Blue=1, Green, Red }
+let color: Color = Color.Blue;
+console.log(color);
+console.log(Color.Green);
+console.log(Color.Red);
+
+let unusable: void = undefined;
---
+++ Codes/TypeScript/hello.ts
@@ -0,0 +1,5 @@
+function hello() {
+    let hello: string = "Hello world!";
+    console.log(hello);
+}
+hello();
